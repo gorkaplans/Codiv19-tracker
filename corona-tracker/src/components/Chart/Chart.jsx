21eba9bb06bba2c66e8a66b0beb0329cccad50af
Gorka Plans <gorkaplans@gmail.com>
2021-04-19 20:26:59 +0200

    start creting chart

--- corona-tracker/src/components/Chart/Chart.jsx
+++ corona-tracker/src/components/Chart/Chart.jsx
@@ -1,8 +1,48 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2'; 
+ 
+import styles from './Chart.module.css'
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState([])
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        console.log(dailyData)
+
+        fetchApi(); 
+    }, [dailyData]);
+
+    const lineChart = (
+        dailyData.length 
+        ? (
+        <Line 
+        data={{
+           labels: dailyData.map(({ date }) => date), 
+           datasets: [{
+                data: dailyData.map(({ confirmed }) => confirmed), 
+                label: 'Infected', 
+                borderColor: '#93C5FD', 
+                fill: true
+           }, {
+                data: dailyData.map(({ deaths }) => deaths), 
+                label: 'Infected', 
+                borderColor: '#F87171', 
+                backgroundColor: '#FDA4AF',
+                fill: true
+           }], 
+        }}
+        />) : null
+    ); 
+
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
